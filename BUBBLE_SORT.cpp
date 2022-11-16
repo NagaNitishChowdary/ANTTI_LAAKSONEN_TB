97cf30efb440f3274cfd17fc38f35efe53fb5a00
NagaNitishChowdary <95007559+NagaNitishChowdary@users.noreply.github.com>
2022-11-16 18:36:29 +0530

    Update BUBBLE_SORT.cpp

--- BUBBLE_SORT.cpp
+++ BUBBLE_SORT.cpp
@@ -18,7 +18,7 @@ void bubblesort(){
     int arr[n];
     for(int i=0;i<n;i++) cin>>arr[i];
     for(int i=0;i<n;i++){
-        for(int j=0;j<n-1;j++){
+        for(int j=0;j<n-i-1;j++){
             if(arr[j]>arr[j+1]){
                 swap(arr[j],arr[j+1]);
             }
@@ -33,4 +33,3 @@ int main(int argc,char const *argv[]) {
     cout.tie(0);
     bubblesort();  
 }
-
